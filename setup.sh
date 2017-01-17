03c84fad0fb5c1475343853bd509b2300352232f
Matthew Spencer <hello@thisismatthew.com>
2017-01-16 16:08:43 -0800

    Adding vim-gitgutter to brain bridge over from Sublime.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/ctrlpvim/ctrlp.vim.git     ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutte
