5dd41e90035273b983cb39db25e1ed8dbae86170
Matthew Spencer <mspencer@etsy.com>
2025-12-17 11:46:36 -0500

    trying out wezterm

---
+++ files/wezterm/.wezterm.lua
@@ -0,0 +1,13 @@
+-- https://wezterm.org/config/files.html#quick-start
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'Dank Mono'
+config.font_size = 15
+config.color_scheme = 'Night Owlish Light'
+
+-- https://wezterm.org/config/keyboard-concepts.html?h=option+alt#layouts-with-an-altgr-key
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+
+return config
